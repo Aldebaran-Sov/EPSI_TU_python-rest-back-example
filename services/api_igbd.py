682b22c4edd3aa176b6a46e93997cf22830048f9
h3lax <axel.poidebard@gmail.com>
2025-03-06 17:04:31 +0100

    add[feat]:get_videogame feat added to the app

--- services/api_igbd.py
+++ services/api_igbd.py
@@ -23,7 +23,24 @@ def get_auth_token() -> str:
     token = response.json().get("access_token")
     return token
 
+def get_headers():
+    load_dotenv()
+    client_id = os.getenv("TWITCH_CLIENT_ID")
+    token = get_auth_token()
+    header = {
+        'Client-ID': client_id,
+        'Authorization': f'Bearer {token}',
+        'Accept': 'application/json'
+    }
+    return header
 
+def get_videogame(name : str):
+    headers = get_headers()
+    url = f"{base_url}/games"
+    payload = f"fields name, genres, first_release_date; search \"{name}\"; where version_parent = null;"
+    response = requests.post(url, headers=headers, data=payload)
+    response.raise_for_status()
+    return response.json()[0]
 
 
 
